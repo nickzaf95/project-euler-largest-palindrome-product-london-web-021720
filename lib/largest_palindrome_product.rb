b63661785edd4ac9bc334ae86b08ac0229061030
nickzaf95 <nicholaszafiriou@gmail.com>
2020-10-06 15:13:08 +0300

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,40 @@
-# Implement your procedural solution here!
+def palindromeCheck(num)
+    n = num.to_s.length
+    x = -1
+    answer = false
+    test = num.to_s
+    for i in 0..2
+        if i == 0
+            x = n -1
+        end
+        if i == 1
+            x = n - 2
+        end
+        if i == 2
+            x = n - 3
+        end
+        if test[i] == test[x]
+            answer = true
+        else
+            return false
+        end
+    end
+    return true
+end
+
+def largest_palindrome_product()
+    answer = 0
+    for i in 100..999
+        for j in 100..999
+            current = i * j
+            if palindromeCheck(current)
+                if (current) > answer
+                    answer = current
+                end
+            end
+        end
+    end
+    print(answer)
+    return answer
+end
+    
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,44 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct 
+
+    def palindromeCheck(num)
+        n = num.to_s.length
+        x = -1
+        test = num.to_s
+        thing = false
+        for i in 0..2
+            if i == 0
+                x = n -1
+            end
+            if i == 1
+                x = n - 2
+            end
+            if i == 2
+                x = n - 3
+            end
+            if test[i] == test[x]
+                thing = true
+            else
+                return false
+            end
+        end
+        return true
+    end
+
+    def answer()
+        ans = 0
+        for i in 100..999
+            for j in 100..999
+                current = i * j
+                if palindromeCheck(current)
+                    if (current) > ans
+                        ans = current
+                    end
+                end
+            end
+        end
+        print(ans)
+        return ans
+    end
+
+
+end
